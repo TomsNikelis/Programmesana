1c7891e91cb6a1b002b1da42ecd75fd8572063b2
Toms Nikelis <nikelistoms@gmail.com>
2022-12-03 19:22:58 +0200

    Forward_Engineer atjaunosana
    Forward_Engineer atjaunosana

--- SQL/Forward_Engineer.sql
+++ SQL/Forward_Engineer.sql
@@ -15,20 +15,6 @@ CREATE SCHEMA IF NOT EXISTS `ITatbalsts` DEFAULT CHARACTER SET utf8 ;
 USE `ITatbalsts` ;
 
 -- -----------------------------------------------------
--- Table `ITatbalsts`.`adrese`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ITatbalsts`.`adrese` (
-  `adrese_id` INT NOT NULL AUTO_INCREMENT,
-  `iela` VARCHAR(45) NOT NULL,
-  `majas_nr` VARCHAR(45) NOT NULL,
-  `dzivokla_nr` VARCHAR(45) NULL,
-  `adresecol` VARCHAR(45) NULL,
-  `pasta_indekss` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`adrese_id`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
 -- Table `ITatbalsts`.`lietotajs`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ITatbalsts`.`lietotajs` (
@@ -39,30 +25,8 @@ CREATE TABLE IF NOT EXISTS `ITatbalsts`.`lietotajs` (
   `epasts` VARCHAR(255) NOT NULL,
   `telefons` VARCHAR(45) NOT NULL,
   `dzim_datums` DATE NOT NULL,
-  `id_adrese` INT NOT NULL,
-  PRIMARY KEY (`lietotajs_id`),
-  CONSTRAINT `fk_lietotajs_adrese1`
-    FOREIGN KEY (`id_adrese`)
-    REFERENCES `ITatbalsts`.`adrese` (`adrese_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `ITatbalsts`.`problema`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `ITatbalsts`.`problema` (
-  `problema_id` INT NOT NULL AUTO_INCREMENT,
-  `apraksts` TEXT NOT NULL,
-  `status` VARCHAR(45) NOT NULL,
-  `id_lietotajs` INT NOT NULL,
-  PRIMARY KEY (`problema_id`),
-  CONSTRAINT `fk_problema_lietotajs`
-    FOREIGN KEY (`id_lietotajs`)
-    REFERENCES `ITatbalsts`.`lietotajs` (`lietotajs_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION)
+  `adrese` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`lietotajs_id`))
 ENGINE = InnoDB;
 
 
@@ -86,23 +50,12 @@ CREATE TABLE IF NOT EXISTS `ITatbalsts`.`darbinieki` (
   `epasts` VARCHAR(255) NOT NULL,
   `telefons` VARCHAR(45) NOT NULL,
   `alga` FLOAT NOT NULL,
-  `problema_id` INT NULL,
+  `adrese` VARCHAR(45) NOT NULL,
   `id_grupa` INT NOT NULL,
-  `id_adrese` INT NOT NULL,
   PRIMARY KEY (`darbinieki_id`),
-  CONSTRAINT `fk_darbinieki_problema1`
-    FOREIGN KEY (`problema_id`)
-    REFERENCES `ITatbalsts`.`problema` (`problema_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
   CONSTRAINT `fk_darbinieki_grupas1`
     FOREIGN KEY (`id_grupa`)
     REFERENCES `ITatbalsts`.`grupas` (`grupas_id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_darbinieki_adrese1`
-    FOREIGN KEY (`id_adrese`)
-    REFERENCES `ITatbalsts`.`adrese` (`adrese_id`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -127,14 +80,56 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
+-- Table `ITatbalsts`.`problema`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ITatbalsts`.`problema` (
+  `problema_id` INT NOT NULL AUTO_INCREMENT,
+  `apraksts` TEXT NOT NULL,
+  `status` VARCHAR(45) NOT NULL,
+  `id_lietotajs` INT NOT NULL,
+  `id_darbinieki` INT NOT NULL,
+  PRIMARY KEY (`problema_id`),
+  CONSTRAINT `fk_problema_lietotajs`
+    FOREIGN KEY (`id_lietotajs`)
+    REFERENCES `ITatbalsts`.`lietotajs` (`lietotajs_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_problema_darbinieki1`
+    FOREIGN KEY (`id_darbinieki`)
+    REFERENCES `ITatbalsts`.`darbinieki` (`darbinieki_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `ITatbalsts`.`premijas`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ITatbalsts`.`premijas` (
   `premijas_id` INT NOT NULL,
+  `premija` FLOAT NOT NULL,
   `status` ENUM('Izdota', 'Nav izdota') NOT NULL,
   `id_darbinieki` INT NOT NULL,
   PRIMARY KEY (`premijas_id`),
   CONSTRAINT `fk_premijas_darbinieki1`
+    FOREIGN KEY (`id_darbinieki`)
+    REFERENCES `ITatbalsts`.`darbinieki` (`darbinieki_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
+-- Table `ITatbalsts`.`atvalinajumi`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `ITatbalsts`.`atvalinajumi` (
+  `atvalinajumi_id` INT NOT NULL AUTO_INCREMENT,
+  `sakuma_datums` DATE NOT NULL,
+  `beigu_datums` DATE NOT NULL,
+  `atmaksats` ENUM('Ja', 'Ne') NOT NULL,
+  `id_darbinieki` INT NOT NULL,
+  PRIMARY KEY (`atvalinajumi_id`),
+  CONSTRAINT `fk_atvalinajumi_darbinieki1`
     FOREIGN KEY (`id_darbinieki`)
     REFERENCES `ITatbalsts`.`darbinieki` (`darbinieki_id`)
     ON DELETE NO ACTION
